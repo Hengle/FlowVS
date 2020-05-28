3c917cb465c31264e404ce3778e0d92ab0ed3b84
JuicyPark <bassoff@naver.com>
2020-05-28 14:36:46 +0900

    DLL : Editor 부분 구현

---
+++ Modules/FlowEditor/View/ConditionView.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+namespace FLOW.Editor.View
+{
+    public abstract class ConditionView
+    {
+        protected GUIStyle nodeLayoutStyle = new GUIStyle();
+        protected GUIStyle inspectorLayoutStyle = new GUIStyle();
+
+        protected ConditionView()
+        {
+            Color conditionColor = new Color(0.4f, 0.4f, 1f);
+            nodeLayoutStyle.alignment = TextAnchor.MiddleCenter;
+            nodeLayoutStyle.normal.textColor = conditionColor;
+            inspectorLayoutStyle.alignment = TextAnchor.MiddleLeft;
+            inspectorLayoutStyle.normal.textColor = conditionColor;
+        }
+
+        public abstract void DisplayInspector(FinishInfo info);
+        public abstract void DisplayNodeView();
+    }
+}
