130893ca9413cad8ca31800ed7dad4da906d4af8
JuicyPark <bassoff@naver.com>
2020-05-29 12:50:03 +0900

    feat : Editor 파티클 부분

---
+++ Assets/Editor/ActionView/Particle/ParticleCreateView.cs
@@ -0,0 +1,25 @@
+﻿using FLOW.Editor.View;
+using FLOW;
+using UnityEngine;
+using UnityEditor;
+
+public class ParticleCreateView : ActionView
+{
+    public override void DisplayInspectorView(ActionInfo info)
+    {
+        EditorGUILayout.LabelField("Particle Create", layoutStyle);
+        info.obj = EditorGUILayout.ObjectField("Particle", info.obj, typeof(GameObject), false) as GameObject;
+        if (!info.obj)
+            return;
+        var component = info.obj.GetComponentInChildren<ParticleSystem>();
+        if (!component)
+            info.obj = null;
+        info.position = EditorGUILayout.Vector3Field("Position", info.position);
+    }
+
+    public override void DisplayNodeView()
+    {
+        GUILayout.Label("Particle Create", layoutStyle);
+    }
+}
+
---
+++ Assets/Editor/ActionView/Particle/ParticleDestroySecondsView.cs
@@ -0,0 +1,26 @@
+﻿using FLOW.Editor.View;
+using FLOW;
+using UnityEngine;
+using UnityEditor;
+
+public class ParticleDestroySecondsView : ActionView
+{
+    public override void DisplayInspectorView(ActionInfo info)
+    {
+        EditorGUILayout.LabelField("Particle Destroy Seconds", layoutStyle);
+        info.obj = EditorGUILayout.ObjectField("Particle", info.obj, typeof(GameObject), false) as GameObject;
+        if (!info.obj)
+            return;
+        var component = info.obj.GetComponentInChildren<ParticleSystem>();
+        if (!component)
+            info.obj = null;    
+        info.position = EditorGUILayout.Vector3Field("Position", info.position);
+        info.floatNumber = EditorGUILayout.FloatField("Seconds", info.floatNumber);
+    }
+
+    public override void DisplayNodeView()
+    {
+        GUILayout.Label("Particle Destroy Seconds", layoutStyle);
+    }
+}
+
