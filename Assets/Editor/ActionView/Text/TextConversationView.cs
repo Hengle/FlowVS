c0ac63267e5ff6ae5a75aa83aad589f1a50c763e
JuicyPark <bassoff@naver.com>
2020-05-29 12:50:33 +0900

    feat : Editor 텍스트 부분

---
+++ Assets/Editor/ActionView/Text/TextConversationView.cs
@@ -0,0 +1,19 @@
+﻿using FLOW.Editor.View;
+using FLOW;
+using UnityEngine;
+using UnityEditor;
+
+public class TextConversationView : ActionView
+{
+    public override void DisplayInspectorView(ActionInfo info)
+    {
+        EditorGUILayout.LabelField("Text Conversation", layoutStyle);
+        info.conversationTemplate = (ConversationTemplate)EditorGUILayout.ObjectField("Conversation Template", info.conversationTemplate, typeof(ConversationTemplate), false);
+    }
+
+    public override void DisplayNodeView()
+    {
+        GUILayout.Label("Text Conversation", layoutStyle);
+    }
+}
+
---
+++ Assets/Editor/ActionView/Text/TextCreateView.cs
@@ -0,0 +1,21 @@
+﻿using FLOW.Editor.View;
+using FLOW;
+using UnityEngine;
+using UnityEditor;
+
+public class TextCreateView : ActionView
+{
+    public override void DisplayInspectorView(ActionInfo info)
+    {
+        EditorGUILayout.LabelField("Text Create", layoutStyle);
+        info.textTemplate = (TextTemplate)EditorGUILayout.ObjectField("Text Template", info.textTemplate, typeof(TextTemplate), false);
+        info.position = EditorGUILayout.Vector3Field("Context Position", info.position);
+        info.context = EditorGUILayout.TextField("Context", info.context);
+    }
+
+    public override void DisplayNodeView()
+    {
+        GUILayout.Label("Text Create", layoutStyle);
+    }
+}
+
