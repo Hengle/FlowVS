9889a276da15e9b891de6ecbaaf8491ebdc24172
JuicyPark <bassoff@naver.com>
2020-05-29 12:49:48 +0900

    feat : Editor 하이어라키 부분

---
+++ Assets/Editor/ActionView/Hierarchy/HiearchyLoadView.cs
@@ -0,0 +1,19 @@
+﻿using FLOW.Editor.View;
+using FLOW;
+using UnityEngine;
+using UnityEditor;
+
+public class HiearchyLoadView : ActionView
+{
+    public override void DisplayInspectorView(ActionInfo info)
+    {
+        EditorGUILayout.LabelField("Hiearchy Load", layoutStyle);
+        info.flowHierarchy = EditorGUILayout.ObjectField("FlowHierarchy", info.flowHierarchy, typeof(FlowHierarchy), false) as FlowHierarchy;
+    }
+
+    public override void DisplayNodeView()
+    {
+        GUILayout.Label("Hiearchy Load", layoutStyle);
+    }
+}
+
