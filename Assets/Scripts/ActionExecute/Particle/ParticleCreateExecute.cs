e6a610c54b2e17b86882a893bcd7fc4e2b53d130
JuicyPark <bassoff@naver.com>
2020-05-30 11:57:15 +0900

    feat : Particle 구현부

---
+++ Assets/Scripts/ActionExecute/Particle/ParticleCreateExecute.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using FLOW;
+using FLOW.Exetue;
+using UnityEngine;
+
+public class ParticleCreateExecute : IActionExecute
+{
+    IEnumerator IActionExecute.ExecuteAction(ActionInfo info, Transform player)
+    {
+        if (info.obj != null)
+        {
+            GameObject obj = Object.Instantiate(info.obj);
+            obj.transform.position = info.position;
+            obj.transform.rotation = Quaternion.Euler(info.rotation);
+            obj.transform.localScale *= info.floatNumber;
+            yield return null;
+        }
+        info.finishInfo.isPlaying = false;
+    }
+
+    void IActionExecute.FinishAction(ActionInfo info, Transform player)
+    {
+        if (info.obj == null)
+            return;
+        GameObject obj = Object.Instantiate(info.obj);
+        obj.transform.position = info.position;
+        obj.transform.rotation = Quaternion.Euler(info.rotation);
+        obj.transform.localScale *= info.floatNumber;
+    }
+}
---
+++ Assets/Scripts/ActionExecute/Particle/ParticleDestroySecondsExecute.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using FLOW;
+using FLOW.Exetue;
+using UnityEngine;
+
+public class ParticleDestroySecondsExecute : IActionExecute
+{
+    IEnumerator IActionExecute.ExecuteAction(ActionInfo info, Transform player)
+    {
+        if (info.obj != null)
+        {
+            GameObject obj = Object.Instantiate(info.obj);
+            obj.transform.position = info.position;
+            obj.transform.rotation = Quaternion.Euler(info.rotation);
+            obj.transform.localScale *= info.floatNumber;
+            yield return new WaitForSeconds(info.floatNumber);
+            Object.Destroy(obj);
+        }
+        info.finishInfo.isPlaying = false;
+    }
+
+    void IActionExecute.FinishAction(ActionInfo info, Transform player)
+    {
+
+    }
+}
