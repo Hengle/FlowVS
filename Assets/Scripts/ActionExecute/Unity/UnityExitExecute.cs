fe8b90b40c38c21ce5be12c7ed9f775a9c620971
JuicyPark <bassoff@naver.com>
2020-05-31 08:51:44 +0900

    feat : 게임 기본 옵션 구현부

---
+++ Assets/Scripts/ActionExecute/Unity/UnityExitExecute.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using FLOW;
+using FLOW.Exetue;
+using UnityEngine;
+
+public class UnityExitExecute : IActionExecute
+{
+    IEnumerator IActionExecute.ExecuteAction(ActionInfo info, Transform player)
+    {
+        yield return null;
+        info.finishInfo.isPlaying = false;
+        Application.Quit();
+    }
+
+    void IActionExecute.FinishAction(ActionInfo info, Transform player)
+    {
+        Application.Quit();
+    }
+}
