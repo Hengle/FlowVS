49b78a8f1b4c92415e68e5ddb80ca4bf0cfd1c51
JuicyPark <bassoff@naver.com>
2020-05-30 11:57:02 +0900

    feat : Hierarchy 구현부

---
+++ Assets/Scripts/ActionExecute/Hierarchy/HierarchyLoadExecute.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using FLOW;
+using FLOW.Exetue;
+using FLOW.Player;
+using UnityEngine;
+
+public class HierarchyLoadExecute : IActionExecute
+{
+    IEnumerator IActionExecute.ExecuteAction(ActionInfo info, Transform player)
+    {
+        if (info.flowHierarchy != null)
+        {
+            var hierarchyPlayer = Object.Instantiate(info.flowHierarchy.hierarchyPlayer);
+            yield return hierarchyPlayer.GetComponent<FlowPlayer>().Play(info.flowHierarchy);
+            yield return null;
+        }
+        info.finishInfo.isPlaying = false;
+    }
+
+    void IActionExecute.FinishAction(ActionInfo info, Transform player)
+    {
+        if (info.flowHierarchy == null)
+            return;
+        var hierarchyPlayer = Object.Instantiate(info.flowHierarchy.hierarchyPlayer);
+        hierarchyPlayer.GetComponent<CustomPlayer>().SetFinish(info.flowHierarchy);
+    }
+}
