a68193481d57487cee1e92b2ca448379c3ba2126
JuicyPark <bassoff@naver.com>
2020-05-30 11:58:44 +0900

    feat : Text 구현부

---
+++ Assets/Scripts/ActionExecute/Text/TextConversationExecute.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using FLOW;
+using FLOW.Exetue;
+using UnityEngine;
+
+
+public class TextConversationExecute : IActionExecute
+{
+    IEnumerator IActionExecute.ExecuteAction(ActionInfo info, Transform player)
+    {
+        if (info.conversationTemplate != null)
+        {
+            var dialogueContainer = player.GetComponentInChildren<CustomDialogueContainer>();
+            yield return dialogueContainer.Converse(info);
+        }
+        info.finishInfo.isPlaying = false;
+    }
+
+    void IActionExecute.FinishAction(ActionInfo info, Transform player)
+    {
+
+    }
+}
+
---
+++ Assets/Scripts/ActionExecute/Text/TextCreateExecute.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using FLOW;
+using FLOW.Exetue;
+using UnityEngine;
+
+
+public class TextCreateExecute : IActionExecute
+{
+    IEnumerator IActionExecute.ExecuteAction(ActionInfo info, Transform player)
+    {
+        if (info.textTemplate)
+        {
+            var dialogueContainer = player.GetComponentInChildren<CustomDialogueContainer>();
+            var text = dialogueContainer.CreateText(info);
+            if (info.textTemplate.isTyping)
+            {
+                for (int i = 0; i < info.context.Length; i++)
+                {
+                    text.text += info.context[i];
+                    yield return new WaitForSeconds(0.025f);
+                }
+            }
+            else
+                text.text = info.context;
+
+            info.finishInfo.isPlaying = false;
+        }
+    }
+
+    void IActionExecute.FinishAction(ActionInfo info, Transform player)
+    {
+        if (!info.textTemplate)
+            return;
+        var dialogueContainer = player.GetComponentInChildren<CustomDialogueContainer>();
+        var text = dialogueContainer.CreateText(info);
+        text.text = info.context;
+    }
+}
+
